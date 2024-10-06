9aced1ac3e52477cdda352ecdcb55facbfa75725
istvzsig <istvanzsigmond@aol.com>
2024-10-06 21:50:42 +0200

    update start script

--- start.sh
+++ start.sh
@@ -1,4 +1,45 @@
 #!/bin/bash
 
-npm i
-npm run dev
+create_components() {
+  components_array=(
+    "GameBoard"
+    "TopicCard"
+    "CodeCard"
+    "ChameleonCard"
+    "Dice"
+    "PlayerList"
+    "GameStatus"
+  )
+
+  components_dir="./src/components"
+
+  mkdir -p $components_dir
+
+  for name in ${components_array[@]}; do
+    file=$components_dir/$name.jsx
+    content=$(
+      cat <<EOF
+export default function $name() {
+  return (
+    <div className="$name">
+      <h1>${name} Component</h1>
+    </div>
+  );
+}
+EOF
+    )
+    touch $file
+    echo "$content" >$file
+  done
+
+  # Format the created files with Prettier
+  npx prettier --write "$components_dir/*.jsx"
+
+  echo "Components created and formatted."
+}
+
+create_components
+
+# Start game
+# npm install
+# npm run dev
